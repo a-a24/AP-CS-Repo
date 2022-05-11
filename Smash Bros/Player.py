b6352b187879d85931cd9820f46733eefac12ee9
Andrew Heine <drewheine@gmail.com>
2022-05-11 13:51:05 -0400

    some physics

--- Smash Bros/Player.py
+++ Smash Bros/Player.py
@@ -4,10 +4,12 @@ class Player:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.vx = 0
+        self.vy = 0
         self.name = "Temporary"
-    
+
     def update(self):
-        self.y += .1
+        self.y += self.vy
 
     def draw(self, screen):
         pygame.draw.rect(screen, (255, 0,0), (self.x, self.y, 10, 10))
