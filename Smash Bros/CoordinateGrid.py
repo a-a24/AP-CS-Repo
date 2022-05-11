ef639551aad3e9d5e2427a3dbb542f24b7d9de3a
John <jyuan23@gds.org>
2022-05-11 13:55:41 -0400

    coordinate grid wip

---
+++ Smash Bros/CoordinateGrid.py
@@ -0,0 +1,21 @@
+import pygame
+
+red = (255,0,0)
+class CoordinateGrid:
+    pixelSize = 0
+    cGrid = []
+    def __init__(self, pSize, xMax, yMax):
+        pixelSize = pSize
+        for i in range(0, yMax):
+            row = []
+            for j in range(0, xMax):
+                row.append(0)
+            self.cGrid.append(row)
+
+    def turnOn(self,x,y):
+        self.cGrid[y][x] = 1
+    def draw(self,screen):
+        for r in range(0,len(self.cGrid)):
+            for c in range(0,len(self.cGrid[r])):
+                if self.cGrid[r][c]>0:
+                    pygame.draw.rect(screen, red, (c*self.pixelSize, r*self.pixelSize, self.pixelSize,self.pixelSize))
