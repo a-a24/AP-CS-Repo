e5d6ef73c7147fab4988efb32d3211f061d9f446
John <jyuan23@gds.org>
2022-05-13 09:56:34 -0400

    edited draw function

--- Smash Bros/Tile.py
+++ Smash Bros/Tile.py
@@ -2,7 +2,7 @@ import pygame
 
 class Tile:
     pixelSize = 10
-    tileType = 0 #0-empty 1-block
+    tileType = 0 #0-air 1-solid block 2-able to jump through, hit down button to go down through
     x = 0
     y = 0
     def __init__(self, xCoord, yCoord, pixelSize = 10, tileType = 0):
@@ -19,8 +19,8 @@ class Tile:
 
     def draw(self, screen):
         if self.tileType == 1:
-            pygame.draw.rect(screen, (0, 0,255), (self.x*self.pixelSize, self.y*self.pixelSize, self.pixelSize, self.pixelSize))
+            pygame.draw.rect(screen, (0, 255,100), (self.x*self.pixelSize, self.y*self.pixelSize, self.pixelSize, self.pixelSize))
         elif self.tileType == 2:
-            pygame.draw.rect(screen, (255, 0,0), (self.x*self.pixelSize, self.y*self.pixelSize, self.pixelSize, self.pixelSize))
+            pygame.draw.rect(screen, (139, 69, 19), (self.x*self.pixelSize, self.y*self.pixelSize, self.pixelSize, self.pixelSize))
         pygame.draw.rect(screen, (50, 50, 50), (self.x*self.pixelSize, self.y*self.pixelSize, self.pixelSize, self.pixelSize), 1)
 
